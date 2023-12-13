Fixes off by one target, ya dingus

yoyodyne/data/batches.py
@@ -98,6 +98,7 @@ class DecoderOnlyPaddedTensor(PaddedTensor):
     padded: torch.Tensor
     mask: torch.Tensor
     prefix_lengths: torch.Tensor
+    is_masked_sequence: Optional[bool]
     is_target: Optional[bool]
 
     def __init__(
@@ -105,38 +106,54 @@ class DecoderOnlyPaddedTensor(PaddedTensor):
         source_tensorlist: List[torch.Tensor],
         target_tensorlist: Optional[List[torch.Tensor]],
         pad_idx: int,
+        is_masked_sequence: Optional[bool]=False,
         is_target: Optional[bool] = False,
         length_msg_callback: Optional[Callable[[int], None]] = None,
         pad_len: Optional[int] = None,
     ):
         super(PaddedTensor, self).__init__()
+        self.is_masked_sequence = is_masked_sequence
         self.is_target = is_target
         # TODO: Ensure we do not have EOS on the source tensors already
         # TODO: Clean this up---make separate methods.
         if target_tensorlist is not None:
-            if pad_len is None:
-                pad_len = max(
-                    len(t1) + len(t2)
-                    for t1, t2 in zip(source_tensorlist, target_tensorlist)
-                )
-            if length_msg_callback is not None:
-                length_msg_callback(pad_len)
-            # Target tensors look the same as the source tensor, but we replace the source info
-            # with PADs for ignoring loss on them.
+            # Target for validation; contains JUST the actual target sequence.
             if self.is_target:
+                if pad_len is None:
+                    pad_len = max(len(t) for t in target_tensorlist)
+                if length_msg_callback is not None:
+                    length_msg_callback(pad_len)
                 tensors = torch.stack(
                     [
-                        self.pad_concat_tensors(t1, t2, pad_idx, pad_len, pad_indices = t1)
-                        for t1, t2 in zip(source_tensorlist, target_tensorlist)
+                        self.pad_tensor(t, pad_idx, pad_len)
+                        for t in target_tensorlist
                     ],
                 )
             else:
-                tensors = torch.stack(
-                    [
-                        self.pad_concat_tensors(t1, t2, pad_idx, pad_len)
+                if pad_len is None:
+                    pad_len = max(
+                        len(t1) + len(t2)
                         for t1, t2 in zip(source_tensorlist, target_tensorlist)
-                    ],
-                )
+                    )
+                if length_msg_callback is not None:
+                    length_msg_callback(pad_len)
+                # During training, target tensors look 
+                # the same as the source tensor, but we replace the source info
+                # with PADs for ignoring loss on them.
+                if self.is_masked_sequence:
+                    tensors = torch.stack(
+                        [
+                            self.pad_concat_tensors(t1, t2, pad_idx, pad_len, pad_indices = t1)
+                            for t1, t2 in zip(source_tensorlist, target_tensorlist)
+                        ],
+                    )
+                else:
+                    tensors = torch.stack(
+                        [
+                            self.pad_concat_tensors(t1, t2, pad_idx, pad_len)
+                            for t1, t2 in zip(source_tensorlist, target_tensorlist)
+                        ],
+                    )
         else:
             if pad_len is None:
                 pad_len = max(len(t) for t in source_tensorlist)
@@ -144,21 +161,49 @@ class DecoderOnlyPaddedTensor(PaddedTensor):
                 length_msg_callback(pad_len)
             tensors = torch.stack(
                 [
-                    self.pad_tensor(t, pad_idx, pad_len)
+                    self.pad_tensor_left(t, pad_idx, pad_len)
                     for t in source_tensorlist
                 ],
             )
         
-        # FIXME: We need to account for the left-paddding we now have.
+        # FIXME: We need to account for the left-padding we now have.
         #       Each batch should have a prefix length of num_pads + prefix_length.
         # pad_lens = torch.sum(tensors == pad_idx, dim=1)
-        self.prefix_lengths = [s.size(0) for s in source_tensorlist]
+        if self.is_target:
+            self.prefix_lengths = [0 for _ in target_tensorlist]
+        else:
+            self.prefix_lengths = [s.size(0) for s in source_tensorlist]
         self.register_buffer(
             "padded",
             tensors,
         )
         self.register_buffer("mask", tensors == pad_idx)
 
+    @staticmethod
+    def pad_tensor_left(
+        tensor: torch.Tensor,
+        pad_idx: int,
+        pad_max: int,
+        pad_indices: Optional[torch.Tensor] = None,
+    ) -> torch.Tensor:
+        """Pads a tensor.
+
+        Args:
+            source_tensor (torch.Tensor).
+            target_tensor (torch.Tensor).
+            pad_idx (int): padding index.
+            pad_max (int): desired tensor length.
+
+        Returns:
+            torch.Tensor.
+        """
+        padding = pad_max - len(tensor)
+        padded = nn.functional.pad(tensor, (padding, 0), "constant", pad_idx)
+        if pad_indices is not None:
+            padded[:padding + len(pad_indices)] = pad_idx
+
+        return padded
+    
     @staticmethod
     def pad_concat_tensors(
         source_tensor: torch.Tensor,
@@ -216,18 +261,24 @@ class PaddedBatch(nn.Module):
 
 
 class DecoderOnlyPaddedBatch(PaddedBatch):
-    """Padded bacth for decoder-only models.
+    """Padded batch for decoder-only models.
 
     This represents a padded batch. It is produced by the collator and fed to
     the trainer. We expect both a source and target tensor, which are then concatenated
     together, and the source tensor is tracked as the 'prefix' to an LM."""
 
-    sequence: PaddedTensor
-    target: PaddedTensor
+    sequence: DecoderOnlyPaddedTensor
+    masked_sequence: DecoderOnlyPaddedTensor
+    # For greedy decoding + eval.
+    # FIXME: Do this more elegantly...
+    source: DecoderOnlyPaddedTensor
+    target: DecoderOnlyPaddedTensor
 
-    def __init__(self, sequence, target):
+    def __init__(self, sequence, masked_sequence, source, target):
         super(PaddedBatch, self).__init__()
         self.register_module("sequence", sequence)
+        self.register_module("masked_sequence", masked_sequence)
+        self.register_module("source", source)
         self.register_module("target", target)
 
     def __len__(self) -> int:

yoyodyne/models/base.py
@@ -634,17 +634,9 @@ class BaseDecoderOnly(BaseEncoderDecoder):
         # -> B x seq_len x target_vocab_size.
         predictions = self(batch)
         # Target is the same as sequence but the prefix is pads.
-        target_padded = batch.target.padded
+        target_padded = batch.masked_sequence.padded
         # -> B x target_vocab_size x seq_len. For loss.
         predictions = predictions.transpose(1, 2)
-        print("TRAIN")
-        print("source")
-        print(batch.sequence.padded[0, :])
-        print("target")
-        print(target_padded[0, :])
-        print("pred")
-        print(predictions[0, :].argmax(dim=1))
-        print("="*30)
         # N = torch.isnan(predictions).sum(dim=2)
         # print(N.sum(dim=1))
         loss = self.loss_func(predictions, target_padded)
@@ -681,13 +673,19 @@ class BaseDecoderOnly(BaseEncoderDecoder):
         # -> B x seq_len x target_vocab_size.
         target_padded = batch.target.padded
         greedy_predictions = self(batch)
-        print("source")
-        print(batch.sequence.padded[0, :])
-        print("target")
-        print(target_padded[0, :])
-        print("pred")
-        print(greedy_predictions[0, :target_padded.size(1)].argmax(dim=1))
-        print("="*30)
+        # print("sequence")
+        # print(batch.sequence.padded[0])
+        # print("source")
+        # print(batch.source.padded[0])
+        # print("target")
+        # print(target_padded[0])
+        # print("pred")
+        # print(greedy_predictions[0, :].argmax(dim=1))
+        # print("pred truncated")
+        # print(greedy_predictions[0, :target_padded.size(1)].argmax(dim=1))
+        # # logging_preds = torch.softmax(greedy_predictions, dim=2)
+        # # print(logging_preds[0, :, 3])
+        # print("="*30)
         val_eval_item = self.evaluator.evaluate(
             greedy_predictions, target_padded, self.end_idx, self.pad_idx
         )

yoyodyne/models/modules/transformer.py
@@ -776,7 +776,6 @@ class TransformerDecoderOnly(TransformerDecoder):
         pad_lengths,
         prefix_lengths,
     ):
-        # FIXME:
         # -> batch_size x seq_len x seq_len.
         causal_mask = self.generate_square_subsequent_mask(
             target_sequence_length
@@ -786,6 +785,7 @@ class TransformerDecoderOnly(TransformerDecoder):
         #   Ideally, we could unmask from last_pad:prefix_length
         for b in range(batch_size):
             causal_mask[b, :(pad_lengths[b] + prefix_lengths[b]), :(pad_lengths[b] + prefix_lengths[b])] = 0.0
+            # causal_mask[b, pad_lengths[b]:(pad_lengths[b]+prefix_lengths[b]-1), pad_lengths[b]:(pad_lengths[b]+prefix_lengths[b]-1)] = 0.0
 
         # Repeats per attention head.
         # return causal_mask.to(self.device).repeat(self.source_attention_heads, 1, 1)
@@ -820,9 +820,8 @@ class TransformerDecoderOnly(TransformerDecoder):
         #     mask=prefix_mask,
         #     src_key_padding_mask=target_mask,
         # )
-        # FOR using TransformerDecoder with dummy memory.
+        # For using TransformerDecoder with dummy memory.
         # memory=torch.ones(target_embedding.size(0), 1, target_embedding.size(2)),
-
         output = self.module(
             tgt=target_embedding,
             tgt_mask=prefix_mask,
@@ -830,26 +829,28 @@ class TransformerDecoderOnly(TransformerDecoder):
         )
         # print("Mask info")
         # prefix_count = (prefix_mask == 0).sum(dim=2)
-        # not_prefix_count = (prefix_mask == float("-inf")).sum(dim=2)
+        # # not_prefix_count = (prefix_mask == float("-inf")).sum(dim=2)
         # tgt_mask_count = (target_mask == 0).sum(dim=1)
-        # tgt_not_mask_count = target_mask.sum(dim=1)
-        # i = 19
-        # print(i)
+        # # tgt_not_mask_count = target_mask.sum(dim=1)
+        # # i = 19
+        # i=7
+        # # print(i)
         # print(prefix_count[i])
-        # print(not_prefix_count[i])
-        # print(tgt_mask_count[i])
-        # print(tgt_not_mask_count[i])
-        # print(target[i])
+        # print(prefix_lengths[i])
+        # # print(not_prefix_count[i])
+        # print("target mask count", tgt_mask_count[i])
+        # # print(tgt_not_mask_count[i])
+        # print("target", target[i])
         # print(target_mask[i])
-        # print("TARGET")
-        # print(target[18, :])
-        # print("CAUSAL MASK")
-        # print(prefix_lengths[18])
-        # # print(prefix_mask[5, :, :])
-        # print(prefix_mask[18])
-        # print((prefix_mask == 0).sum(dim=2)[18])
-        # print(target_mask[18])
-        # print(output[18])
+        # # print("TARGET")
+        # # print(target[18, :])
+        # # print("CAUSAL MASK")
+        # # print(prefix_lengths[18])
+        # # # print(prefix_mask[5, :, :])
+        # # print(prefix_mask[18])
+        # # print((prefix_mask == 0).sum(dim=2)[18])
+        # # print(target_mask[18])
+        # # print(output[18])
 
         # print("NAN ONE")
         # print("TARGET")

yoyodyne/models/transformer.py
@@ -264,9 +264,11 @@ class DecoderOnlyTransformer(base.BaseDecoderOnly):
         for _ in range(self.max_target_length - sequence.size(1)):
             if pred is not None:
                 sequence = torch.cat((sequence, pred.unsqueeze(1)), dim=1)
-            # Uses a dummy mask of all ones.
-            sequence_mask = torch.ones_like(sequence, dtype=torch.float)
-            sequence_mask = sequence_mask == 0
+                # Appends to key padding mask.
+                sequence_mask = torch.cat(
+                    (sequence_mask, torch.zeros((batch_size, 1), dtype=torch.float)),
+                    dim=1
+                )
             # TODO: Here we shouldn't need a causal mask at all.
             #       at each timestep, we can attend to everything.
             decoder_output = self.decoder(
@@ -276,6 +278,12 @@ class DecoderOnlyTransformer(base.BaseDecoderOnly):
             ).output
             logits = self.classifier(decoder_output)
             last_output = logits[:, -1, :]  # Ignores EOS.
+            # In the first iteration, we get all outputs, as we want to include
+            # the prefix.
+            # if pred is None:
+            #     outputs.extend(list(logits.transpose(0,1)))
+            # else:
+            #     outputs.append(last_output)
             outputs.append(last_output)
             # -> B x 1 x 1
             _, pred = torch.max(last_output, dim=1)
@@ -325,15 +333,13 @@ class DecoderOnlyTransformer(base.BaseDecoderOnly):
                 batch.sequence.padded, batch.sequence.mask, batch.sequence.prefix_lengths
             ).output
             logits = self.classifier(decoder_output)
-            # FIXME: we do not want to backprop the prefix predicitons as they are given already.
-            # Actually this should break the loss func, which expects the size of the targets.
-            output = logits #logits[:, :-1, :]  # Ignore EOS.
+            output = logits[:, :-1, :]  # Ignore EOS.
         else:
             # -> B x seq_len x output_size.
             output = self._decode_greedy(
-                batch.sequence.padded,
-                batch.sequence.mask,
-                batch.sequence.prefix_lengths,
+                batch.source.padded,
+                batch.source.mask,
+                batch.source.prefix_lengths,
             )
         return output
 