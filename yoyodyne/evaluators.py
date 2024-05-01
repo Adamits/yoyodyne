77bbafb067061dccb5a03eb380e2d81c67fdb654
Adamits <ajwieme@gmail.com>
2024-05-01 16:42:52 -0600

    change name to metrics

--- yoyodyne/evaluators.py
+++ yoyodyne/evaluators.py
@@ -3,6 +3,7 @@
 from __future__ import annotations
 import argparse
 import dataclasses
+import statistics
 from typing import List
 
 import numpy
@@ -19,12 +20,12 @@ class Error(Exception):
 
 @dataclasses.dataclass
 class EvalItem:
-    per_sample_metrics: List[float]
+    per_sample_metrics: numpy.ndarray
 
     @property
-    def average_metric(self) -> float:
+    def metric(self) -> float:
         """Computes the micro-average of the metric."""
-        return sum(self.per_sample_metrics) / len(self.per_sample_metrics)
+        return self.per_sample_metrics.mean()
 
     def __add__(self, other_eval: EvalItem) -> EvalItem:
         """Adds two EvalItem by concatenating the list of individual metrics.
@@ -36,7 +37,7 @@ class EvalItem:
             EvalItem.
         """
         return EvalItem(
-            self.per_sample_metrics + other_eval.per_sample_metrics
+            numpy.concatenate((self.per_sample_metrics, other_eval.per_sample_metrics))
         )
 
     def __radd__(self, start_int: int) -> EvalItem:
@@ -167,7 +168,7 @@ class AccuracyEvaluator(Evaluator):
             )
         # Gets the count of exactly matching tensors in the batch.
         # -> B.
-        accs = (predictions.to(golds.device) == golds).all(dim=1).tolist()
+        accs = (predictions.to(golds.device) == golds).all(dim=1).numpy()
         return EvalItem(accs)
 
     def finalize_predictions(
@@ -312,6 +313,92 @@ class CEREvaluator(Evaluator):
         return "cer"
 
 
+class AccuracyEvaluator(Evaluator):
+    """Evaluates accuracy."""
+
+    def get_eval_item(
+        self,
+        predictions: torch.Tensor,
+        golds: torch.Tensor,
+        pad_idx: int,
+    ) -> EvalItem:
+        if predictions.size(1) > golds.size(1):
+            predictions = predictions[:, : golds.size(1)]
+        elif predictions.size(1) < golds.size(1):
+            num_pads = (0, golds.size(1) - predictions.size(1))
+            predictions = functional.pad(
+                predictions, num_pads, "constant", pad_idx
+            )
+        # Gets the count of exactly matching tensors in the batch.
+        # -> B.
+        accs = (predictions.to(golds.device) == golds).all(dim=1).tolist()
+        return EvalItem(accs)
+
+    def finalize_predictions(
+        self,
+        predictions: torch.Tensor,
+        end_idx: int,
+        pad_idx: int,
+    ) -> torch.Tensor:
+        """Finalizes predictions.
+
+        Cuts off tensors at the first end_idx, and replaces the rest of the
+        predictions with pad_idx, as these are erroneously decoded while the
+        rest of the batch is finishing decoding.
+
+        Args:
+            predictions (torch.Tensor): prediction tensor.
+            end_idx (int).
+            pad_idx (int).
+
+        Returns:
+            torch.Tensor: finalized predictions.
+        """
+        # Not necessary if batch size is 1.
+        if predictions.size(0) == 1:
+            return predictions
+        for i, prediction in enumerate(predictions):
+            # Gets first instance of EOS.
+            eos = (prediction == end_idx).nonzero(as_tuple=False)
+            if len(eos) > 0 and eos[0].item() < len(prediction):
+                # If an EOS was decoded and it is not the last one in the
+                # sequence.
+                eos = eos[0]
+            else:
+                # Leaves predictions[i] alone.
+                continue
+            # Hack in case the first prediction is EOS. In this case
+            # torch.split will result in an error, so we change these 0's to
+            # 1's, which will make the entire sequence EOS as intended.
+            eos[eos == 0] = 1
+            symbols, *_ = torch.split(prediction, eos)
+            # Replaces everything after with PAD, to replace erroneous decoding
+            # While waiting on the entire batch to finish.
+            pads = (
+                torch.ones(
+                    len(prediction) - len(symbols), device=symbols.device
+                )
+                * pad_idx
+            )
+            pads[0] = end_idx
+            # Makes an in-place update to an inference tensor.
+            with torch.inference_mode():
+                predictions[i] = torch.cat((symbols, pads))
+        return predictions
+
+    def finalize_golds(
+        self,
+        golds: torch.Tensor,
+        *args,
+        **kwargs,
+    ):
+        return golds
+
+    @property
+    def metric_name(self):
+        return "accuracy"
+    
+    
 _eval_factory = {
     "accuracy": AccuracyEvaluator,
     "cer": CEREvaluator,
